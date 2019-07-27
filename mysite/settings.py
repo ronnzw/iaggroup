c5e7bbfe259eac6e01e512c1025eb5dfc4f49662
Rmaravanyika <rmaravanyika.rm@gmail.com>
2019-07-27 23:02:00 +0200

    adding statics

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2o@%dip+a3v(!&=vn0=z*&mwt7(7uj2a6#a4q%gricpb=*n1%)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
