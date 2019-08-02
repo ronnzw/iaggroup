4e8934b0959f7776f33146cb259e28c968abc364
Rmaravanyika <rmaravanyika.rm@gmail.com>
2019-08-02 13:31:46 +0200

    adding admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
+
+
+class ContactAdmin(admin.ModelAdmin):
+	list_display = ('name', 'email', 'phone', 'subject', 'message')
+
+
+class FreeQuoteAdmin(admin.ModelAdmin):
+	list_display = ('name', 'surname', 'email', 'cell_number', 'address', 'additional_info', 'location')
+
+admin.site.register(Post)
+admin.site.register(Newletter)
+admin.site.register(FreeQuote, FreeQuoteAdmin)
+admin.site.register(Contact, ContactAdmin)
+# Register your models here.
 
 # Register your models here.
